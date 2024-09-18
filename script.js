2577d8a2c8d4bc7b37a42fa85682d4ef65bde9a1
ImanBlossom <160495466+ImanBlossom@users.noreply.github.com>
2024-09-18 19:25:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,137 @@
+document.getElementById('9').onmouseover = function(){
+	document.getElementById('9').style.backgroundColor = "cyan"
+}
+
+document.getElementById('9').onmouseleave = function(){
+	document.getElementById('9').style.backgroundColor = "rgba(0, 0, 0, 0.1)"
+}
+
+document.getElementById('8').onmouseover = function(){
+	document.getElementById('8').style.backgroundColor = "cyan"
+}
+
+document.getElementById('8').onmouseleave = function(){
+	document.getElementById('8').style.backgroundColor = "rgba(0, 0, 0, 0.1)"
+}
+document.getElementById('7').onmouseover = function(){
+	document.getElementById('7').style.backgroundColor = "cyan"
+}
+
+document.getElementById('7').onmouseleave = function(){
+	document.getElementById('7').style.backgroundColor = "rgba(0, 0, 0, 0.1)"
+}
+
+document.getElementById('c').onmouseover = function(){
+	document.getElementById('c').style.backgroundColor = "cyan"
+}
+
+document.getElementById('c').onmouseleave = function(){
+	document.getElementById('c').style.backgroundColor = "rgba(0, 0, 0, 0.1)"
+}
+
+document.getElementById('6').onmouseover = function(){
+	document.getElementById('6').style.backgroundColor = "deepskyblue"
+}
+
+document.getElementById('6').onmouseleave = function(){
+	document.getElementById('6').style.backgroundColor = "rgba(0, 0, 0, 0.1)"
+}
+
+document.getElementById('5').onmouseover = function(){
+	document.getElementById('5').style.backgroundColor = "deepskyblue"
+}
+
+document.getElementById('5').onmouseleave = function(){
+	document.getElementById('5').style.backgroundColor = "rgba(0, 0, 0, 0.1)"
+}
+
+document.getElementById('4').onmouseover = function(){
+	document.getElementById('4').style.backgroundColor = "deepskyblue"
+}
+
+document.getElementById('4').onmouseleave = function(){
+	document.getElementById('4').style.backgroundColor = "rgba(0, 0, 0, 0.1)"
+}
+
+document.getElementById('add').onmouseover = function(){
+	document.getElementById('add').style.backgroundColor = "deepskyblue"
+}
+
+document.getElementById('add').onmouseleave = function(){
+	document.getElementById('add').style.backgroundColor = "rgba(0, 0, 0, 0.1)"
+}
+
+
+document.getElementById('3').onmouseover = function(){
+	document.getElementById('3').style.backgroundColor = "royalblue"
+}
+
+document.getElementById('3').onmouseleave = function(){
+	document.getElementById('3').style.backgroundColor = "rgba(0, 0, 0, 0.1)"
+}
+
+document.getElementById('2').onmouseover = function(){
+	document.getElementById('2').style.backgroundColor = "royalblue"
+}
+
+document.getElementById('2').onmouseleave = function(){
+	document.getElementById('2').style.backgroundColor = "rgba(0, 0, 0, 0.1)"
+}
+
+document.getElementById('1').onmouseover = function(){
+	document.getElementById('1').style.backgroundColor = "royalblue"
+}
+
+document.getElementById('1').onmouseleave = function(){
+	document.getElementById('1').style.backgroundColor = "rgba(0, 0, 0, 0.1)"
+}
+
+document.getElementById('min').onmouseover = function(){
+	document.getElementById('min').style.backgroundColor = "royalblue"
+}
+
+document.getElementById('min').onmouseleave = function(){
+	document.getElementById('min').style.backgroundColor = "rgba(0, 0, 0, 0.1)"
+}
+
+
+document.getElementById('.').onmouseover = function(){
+	document.getElementById('.').style.backgroundColor = "blue"
+}
+
+document.getElementById('.').onmouseleave = function(){
+	document.getElementById('.').style.backgroundColor = "rgba(0, 0, 0, 0.1)"
+}
+
+document.getElementById('0').onmouseover = function(){
+	document.getElementById('0').style.backgroundColor = "blue"
+}
+
+document.getElementById('0').onmouseleave = function(){
+	document.getElementById('0').style.backgroundColor = "rgba(0, 0, 0, 0.1)"
+}
+
+document.getElementById('mult').onmouseover = function(){
+	document.getElementById('mult').style.backgroundColor = "blue"
+}
+
+document.getElementById('mult').onmouseleave = function(){
+	document.getElementById('mult').style.backgroundColor = "rgba(0, 0, 0, 0.1)"
+}
+
+document.getElementById('divi').onmouseover = function(){
+	document.getElementById('divi').style.backgroundColor = "blue"
+}
+
+document.getElementById('divi').onmouseleave = function(){
+	document.getElementById('divi').style.backgroundColor = "rgba(0, 0, 0, 0.1)"
+}
+
+document.getElementById('eq').onmouseover = function(){
+	document.getElementById('eq').style.backgroundColor = "darkblue"
+}
+
+document.getElementById('eq').onmouseleave = function(){
+	document.getElementById('eq').style.backgroundColor = "rgba(0, 0, 0, 0.1)"
+}
+
